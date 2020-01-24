191e8974acd5b321f4d184fa6feaca2ee11f0f07
xVHY <cuzlocked@gmail.com>
2020-01-24 12:10:27 +0800

    Added username and real name validation, getUser function and get_data endpoint, new_user now checks that the username and real name are valid and that the username is not taken

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,8 @@ const config = require("../../config")
 var fs = require('fs');
 
 //TODO: Switch some methods to POST instead of GET, I used GET for testing as it's easier to send GET requests from a browser
+//TODO: Create a better logging function that outputs errors to stdout and appends them to file for easier debugging
+//TODO: Fix inconsistent casing, eg. validateRealName(real_name)
 
 //To parse POST url-encoded bodies
 var bodyParser = require('body-parser')
@@ -11,9 +13,34 @@ routes.use(bodyParser.urlencoded({
 }));
 
 dataPath = process.cwd() + "/" + config.data_location + "/"
+dataFile = dataPath + "users"
+
+function validateUsername(username) {
+  username_regex = config.validation.username_regex
+  if (!username) {
+    return "Please provide a username"
+  } else if (!username_regex.test(username)) {
+    return "Invalid username! Only alphanumeric characters and underscores are allowed, 3-20 characters."
+  }
+  return ""
+}
+
+function validateRealName(real_name) {
+  real_name_regex = config.validation.real_name_regex
+  if (!real_name) {
+    return "Please provide a name!"
+  } else if (!real_name_regex.test(real_name)) {
+    return "Invalud name! Only a-z, spaces and dashes are allowed, 2-20 characters."
+  }
+  return ""
+}
+
+function removeBlank(data) {
+  return data.filter((element) => { return element != "" })
+}
 
 function getData(callback, include_data = true) {
-  fs.readFile(dataPath + "data", function(err, data) {
+  fs.readFile(dataFile, function(err, data) {
     if (err) {
       callback(err,data)
     } else {
@@ -21,15 +48,17 @@ function getData(callback, include_data = true) {
       //Get users array from list of lines
       users = users.split("\n")
       //Remove empty lines as they can mess with the parsing
-      users = users.filter((line) => { return line != "" })
+      users = removeBlank(users)
       //Construct JSON array from lines
       var json_data = users.map((line) => {
         var user_data = {}
         user_data.username = line.split(":")[0]
+        user_data.real_name = line.split(":")[1]
         //Get list of when coffee was drank for user
         if (include_data) {
           //Only add list of timestamps to response if include_data is true
-          user_data.data = line.split(":")[1].split(",")
+          user_data.data = line.split(":")[2].split(",")
+          user_data.data = removeBlank(user_data.data)
         }
         return user_data
       })
@@ -37,6 +66,27 @@ function getData(callback, include_data = true) {
     }
   });
 
+}
+
+function getUser(username, real_name, callback) {
+  getData(function(err, data) {
+    if (err) {
+      callback(err, data)
+    } else {
+      //Filter all results down to the desired user
+      if (username) {
+        data = data.filter((user) => { return user.username == username })
+      }
+      if (real_name) {
+        data = data.filter((user) => { return user.real_name == real_name })
+      }
+      if (data.length == 0) {
+        callback(err, [])
+      } else {
+        callback(err, data[0])
+      }
+    }
+  }, true)
 }
 
 routes.get('/users', (req, res) => {
@@ -63,8 +113,46 @@ routes.get('/data', (req, res) => {
   }, true)
 });
 
+routes.get('/get_user', (req, res) => {
+  getUser(req.query.username, req.query.real_name, function(err, data) {
+    if (err) {
+      res.status(500).json({success: false, err: error})
+    } else {
+      if (data.length == 0) {
+        res.status(200).json({success: false, error: "User not found!", user: []})
+      } else {
+        res.status(200).json({success: true, user: data})
+      }
+    }
+  })
+});
+
 routes.get('/new_user', (req, res) => {
-  res.json({name: req.query.name})
+  error = validateUsername(req.query.username)
+  if (!error) {
+    error = validateRealName(req.query.real_name)
+  }
+  if (error) {
+    res.status(400).json({success: false, error: error})
+  } else {
+    getUser(req.query.username, null, function(err, data) {
+      if (data.length != 0) {
+        res.status(400).json({success: false, error: "Username is taken!"})
+      } else {
+        //Construct base line for new user
+        line = req.query.username + ":" + req.query.real_name + ":\n"
+        fs.appendFile(dataFile, line, function (err) {
+          if (err) {
+            console.log("/api/new_user: Error appending to users file!")
+            console.log(err)
+            res.status(500).json({success: false, error: err})
+          } else {
+            res.status(200).json({success: true})
+          }
+        });
+      }
+    })
+  }
 })
 
 
