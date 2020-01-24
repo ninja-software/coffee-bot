144f523e85db2617b1e9f26f5b9f5f1fd60e8fe1
xVHY <cuzlocked@gmail.com>
2020-01-24 21:39:47 +0800

    Add body-parser to API so we can process POST requests.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const routes = require('express').Router();
 const api = require("./api")
 
+//Create body-parser instance to parse application/x-www-form-urlencoded content
+var bodyParser = require('body-parser').urlencoded({ extended: true })
+
 routes.get('/', (req, res) => {
   res.render("index")
 });
 
-routes.use('/api', api)
+//Make sure to use the body parser in the API!
+routes.use('/api', bodyParser, api)
 
 module.exports = routes;
