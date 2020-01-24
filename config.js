1b32e02fdb42b9f3da2b238ff75df7d4440f094d
xVHY <cuzlocked@gmail.com>
2020-01-24 12:11:02 +0800

    Added name validation regular expressions

--- config.js
+++ config.js
@@ -3,5 +3,10 @@ var config = {}
 
 config.secrets = require("./secrets")
 config.data_location = "data_directory"
+//When changing any of these, make sure to update the failed validation error in the validate_username and validate_real_name functions
+config.validation = {
+  username_regex: /^\w{3,20}$/,
+  real_name_regex: /^([a-zA-Z -]{2,20})$/
+}
 
 module.exports = config
