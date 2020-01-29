6c410ba7c2cc41f825aca3983540cd369c7992ac
xVHY <cuzlocked@gmail.com>
2020-01-29 16:29:50 +0800

    Basic update script

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+location="$(pwd)"
+git clone https://github.com/ninja-software/coffee-bot /tmp/coffee-bot
+mkdir /tmp/data_directory
+cp -R data_directory/* /tmp/data_directory
+cp secrets.js /tmp/secrets.js
+rm -rf $location
+mkdir -p $location
+cp -R /tmp/coffee_bot/* $location
+mkdir data_directory
+cp -R /tmp/data_directory/* data_directory
+cp /tmp/secrets.js secrets.js
