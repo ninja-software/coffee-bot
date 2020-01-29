1ecaf381b6c710be75c7272dcb8da043318d6179
xVHY <cuzlocked@gmail.com>
2020-01-29 10:04:38 +0800

    Shell script to install and setup coffee bot.

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+npm install
+npm install --save -g nodemon
+mkdir ./data_directory
+touch ./data_directory/users
+location=$(pwd)
+location="${location//\//\\\/}"
+sed "s/{PATH}/$location/g" coffeetrack.service>coffeereplaced.service
+cp coffeereplaced.service /etc/systemd/system/coffeetracker.service
+systemctl enable coffeetracker.service
+systemctl start coffeetracker.service
+rm coffeereplaced.service
