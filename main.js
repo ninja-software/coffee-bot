67fa85d7db48e87612403556a487c8f8e6f05b26
xVHY <cuzlocked@gmail.com>
2020-01-29 17:16:52 +0800

    Serve /res

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ const app = express()
 const routes = require('./routes');
 var path = require('path')
 
+var views = path.join(__dirname, './views')
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './views'))
+app.set('views', views)
 
 app.use('/', routes);
+app.get("/res/:name", (req, res) => {
+  res.sendFile(views + "/res/" + req.params.name)
+})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000');
