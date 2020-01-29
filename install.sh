3545e3d030f7cb475d4cc16b53834d1f971d382a
xVHY <cuzlocked@gmail.com>
2020-01-29 16:10:38 +0800

    Update install script to not create files as root

--- install.sh
+++ install.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
-if [[ "$EUID" -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
 npm install
-npm install --save -g nodemon
 mkdir ./data_directory
 touch ./data_directory/users
 location=$(pwd)
 location="${location//\//\\\/}"
 sed "s/{PATH}/$location/g" coffeetrack.service>coffeereplaced.service
-cp coffeereplaced.service /etc/systemd/system/coffeetracker.service
-systemctl enable coffeetracker.service
-systemctl start coffeetracker.service
+sudo -u root -H sh -c "npm install --save -g nodemon"
+sudo -u root -H sh -c "cp coffeereplaced.service /etc/systemd/system/coffeetracker.service"
+sudo -u root -H sh -c "systemctl enable coffeetracker.service"
+sudo -u root -H sh -c "systemctl start coffeetracker.service"
 rm coffeereplaced.service
