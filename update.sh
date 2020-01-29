d911f022631c32e75d5c603c6006fa464fbcf871
xVHY <cuzlocked@gmail.com>
2020-01-29 16:44:44 +0800

    Delete previous update cache and use location of relative path for data_directory

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 location="$(pwd)"
 rm -rf /tmp/coffee-bot
 rm /tmp/secrets.js
+rm -rf /tmp/data_directory
 git clone https://github.com/ninja-software/coffee-bot /tmp/coffee-bot
 mkdir /tmp/data_directory
 cp -R data_directory/* /tmp/data_directory
@@ -9,6 +10,6 @@ cp ./secrets.js /tmp/secrets.js
 rm -rf $location
 mkdir -p $location
 cp -R /tmp/coffee-bot/* $location
-mkdir ./data_directory
+mkdir $location/data_directory
 cp -R /tmp/data_directory/* $location/data_directory
 cp /tmp/secrets.js $location/secrets.js
