febcf29b09bf43c947cbe2a6e4e94a68d25d3f1e
xVHY <cuzlocked@gmail.com>
2020-01-29 16:56:28 +0800

    Add admin and backups routes

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,12 @@ var bodyParser = require('body-parser').urlencoded({ extended: true })
 routes.get('/', (req, res) => {
   res.render("index")
 });
+routes.get('/admin', (req, res) => {
+  res.render("admin")
+});
+routes.get('/backups', (req, res) => {
+  res.render("backup")
+});
 
 //Make sure to use the body parser in the API!
 routes.use('/api', bodyParser, api)
