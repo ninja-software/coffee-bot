46cbd6a7459d2cee0f61cddf9828680081be139c
xVHY <cuzlocked@gmail.com>
2020-01-30 09:26:55 +0800

    Make use of express-session for logins and logouts of the admin panel.

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express()
 const routes = require('./routes');
 var path = require('path')
+var config = require("./config")
+
+var session = require('express-session');
+app.use(session({
+  secret: config.secrets.session_key,
+  maxAge: 1*60*60*1000
+}));
 
 var views = path.join(__dirname, './views')
 app.set('view engine', 'ejs');
