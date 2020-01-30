2de0218fc7eafb6a58d7620ce7f2955df0c446ac
xVHY <cuzlocked@gmail.com>
2020-01-30 09:28:15 +0800

    Show different page at /admin depending on whether or not you are logged in.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const routes = require('express').Router();
 const api = require("./api")
+const rate_limit = require("../rate_limit")
 
 //Create body-parser instance to parse application/x-www-form-urlencoded content
 var bodyParser = require('body-parser').urlencoded({ extended: true })
@@ -8,13 +9,15 @@ routes.get('/', (req, res) => {
   res.render("index")
 });
 routes.get('/admin', (req, res) => {
-  res.render("admin")
-});
-routes.get('/backups', (req, res) => {
-  res.render("backup")
+  if (req.session.admin) {
+    res.render("admin")
+  } else {
+    res.render("login")
+  }
 });
 
 //Make sure to use the body parser in the API!
+routes.use('/api', rate_limit, api)
 routes.use('/api', bodyParser, api)
 
 module.exports = routes;
