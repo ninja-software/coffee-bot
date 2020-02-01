44fb575edbf857ed482dd2ae00f4e2d44395fa61
xVHY <cuzlocked@gmail.com>
2020-02-01 18:13:00 +0800

    Allow app to start without Telegram API key, basic automatic backup system.

--- main.js
+++ main.js
@@ -5,19 +5,22 @@ var path = require('path')
 var config = require("./config")
 
 var session = require('express-session');
-if (config.secrets.telegram_api_key == "<telegram-api-key>") { 
-  throw new Error("Please place your Telegram API key in secrets.js!")
+if (config.secrets.telegram_api_key == "<telegram-api-key>") {
+  console.error("\x1b[31m" + "Telegram API key is still default, Telegram integration will be disabled." + "\x1b[0m")
 }
 if (config.secrets.session_key == "<session-key>") {
   throw new Error("Refusing to use default as session key.")
 }
-if (config.secrets.admin_password == "<admin-password>") { 
+if (config.secrets.admin_password == "<admin-password>") {
   throw new Error("Refusing to use default as admin password.")
 }
 app.use(session({
   secret: config.secrets.session_key,
   maxAge: config.session_length
 }));
+
+var backup = require("./backup")
+setInterval(backup, config.backup_frequency)
 
 var views = path.join(__dirname, './views')
 app.set('view engine', 'ejs');
